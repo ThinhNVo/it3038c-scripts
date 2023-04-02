4d5d4a1a3eae56ee8da790ca72b62091c4ca15c0
ThinhNVo <voti@mail.uc.edu>
2023-04-02 01:56:39 -0400

    fixed bug

--- python/apiCaller.py
+++ python/apiCaller.py
@@ -1,7 +1,11 @@
 import json, requests, subprocess, os 
 
 #Enter your local directory for your api program
-localPath = r'C:\Users\thinh'
+print("Enter the your local path to the api program in file with name it3038c-scripts: ")
+
+localPath = input()
+if len(localPath.strip()) == 0:
+    localPath = r'C:\Users\thinh'
 
 #Open api process in the local directory
 api = subprocess.Popen(['node', fr'{localPath}\it3038c-scripts\node\api.js'])
